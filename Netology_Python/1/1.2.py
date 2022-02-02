68c96cc65ddd3b240886341bc618dd2898ef323e
Kostarev Vasiliy <kosva@mail.ru>
2022-02-02 18:35:38 +0300

    Python1v1

---
+++ Netology_Python/1/1.2.py
@@ -0,0 +1,14 @@
+date = input(" Введите дату: ")
+task = input(" Введите задачу: ")
+
+
+date2 = input(" Введите дату: ")
+task2 = input(" Введите задачу: ")
+
+
+date3 = input(" Введите дату: ")
+task3 = input(" Введите задачу: ")
+
+print(date, task)
+print(date2, task2)
+print(date3, task3)
---
+++ Netology_Python/1/1.3.py
@@ -0,0 +1,17 @@
+
+dictionary = {}
+
+date = input(" Введите дату: ")
+task = input(" Введите задачу: ")
+
+
+date2 = input(" Введите дату: ")
+task2 = input(" Введите задачу: ")
+
+date3 = input(" Введите дату: ")
+task3 = input(" Введите задачу: ")
+
+dictionary [date] = task
+dictionary [date2] = task2
+dictionary [date3] = task3
+
