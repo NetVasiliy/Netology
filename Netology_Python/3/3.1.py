9f1f90e4eb3027192c914fa3c5aaa727b982b718
Kostarev Vasiliy <kosva@mail.ru>
2022-02-04 15:45:36 +0300

    Python3v1

---
+++ Netology_Python/3/3.1.py
@@ -0,0 +1,18 @@
+
+run = True
+count = []
+list = []
+
+def count_letter(list, s) :
+  for s in list :
+    count = [w]
+
+while run =True :
+l = input("Введите слово: ")
+  if l !=stop
+    list.append(l)
+  else run = False  
+
+s = input("Введите символ: ")
+
+print(len(count))
