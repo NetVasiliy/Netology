167e211b724aae45948ffb4e0ccfb6702562bcd9
Kostarev Vasiliy <kosva@mail.ru>
2022-02-07 16:32:08 +0300

    Python4v2

--- Netology_Python/4/4.py
+++ Netology_Python/4/4.py
@@ -1,12 +1,14 @@
 import telebot
 
-token = '5240966312:AAGpfBqxEiHgAmIA_8LJBHO3-8tRfHsLU3w'
+token = '_5240966312:AAGpfBqxEiHgAmIA_8LJBHO3-8tRfHsLU3w'
 
 bot = telebot.TeleBot(token)
-
+name = "Vasiya"
 
 @bot.message_handler(content_types=["text"])
 def echo(message):
+    if name in message.text:
+        text = 'Ба! Знакомые все лица'
     bot.send_message(message.chat.id, message.text)
 
 
