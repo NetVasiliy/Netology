dce2cb14bf045a0b5dfc2167f15318a0ce58a151
Kostarev Vasiliy <kosva@mail.ru>
2022-02-07 16:39:19 +0300

    Python4v4

--- Netology_Python/4/4.py
+++ Netology_Python/4/4.py
@@ -3,13 +3,15 @@ import telebot
 token = '_5240966312:AAGpfBqxEiHgAmIA_8LJBHO3-8tRfHsLU3w'
 
 bot = telebot.TeleBot(token)
-name = "Vasiya"
+
+name = 'zz'
 
 @bot.message_handler(content_types=["text"])
 def echo(message):
     if name in message.text:
         text = 'Ба! Знакомые все лица'
-    bot.send_message(message.chat.id, message.text)
-
+    else:
+        text = message.text
+    bot.send_message(message.chat.id, text)
 
 bot.polling(none_stop=True)
